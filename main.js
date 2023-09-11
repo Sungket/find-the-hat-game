20db1dfadcfc98edfae6072bfc1ab0024d5820e1
sungketpatel <93491754+Sungket@users.noreply.github.com>
2023-09-11 10:11:34 +0100

    Added helper method to build a field area

--- main.js
+++ main.js
@@ -17,11 +17,7 @@ class Field {
     //Don't get it twisted with the hardcoded field!
     print() {
         console.log(this._field.join('\n'));
-    }
-
-    static generateField(height, width) {
-        
-    }
+    }  
 }
 
 //initialise win/lose variables as false 
@@ -37,6 +33,29 @@ const myField = [
     [hole, '░', '░', '░', hole]
 ]
 
+function generateField(height, width) {
+
+    newField = [];
+    //set the height by adding the number of arrays specified to build up the height
+    for(let i = 0; i < height; i++) {
+        newField.push([]);
+    }
+    //then, assign each element as randomly as possible, but keeping the ratio appropriate.
+    newField.forEach(heightSubArray => {
+        for(let j = 0; j < width; j++) {
+            let randomNum = Math.floor((Math.random() * 5) +1);
+            if (randomNum === 1) {
+                heightSubArray.push(hole);
+            } else if (randomNum === 2 || randomNum === 3 || randomNum === 4 || randomNum === 5) {
+                heightSubArray.push(fieldCharacter);
+            }
+        }
+    })
+    //this makes the grid look more better, and aligns it to 10 elements wide.
+    newField = newField.join('\n');
+
+}  
+
 //helper method to decide whether user is on a win or lose
 function winOrLose() {
     if(win) {
@@ -50,20 +69,22 @@ function winOrLose() {
 function check() {
     if (x <= 0 || x > myField.length || y < 0 || y === (myField[y].length - 1)) {
         lose = true;
-        console.log('you are out of bounds!')
+        console.log('you are out of bounds! You lose.')
     }
     else if(myField[x][y] === hole) {
         lose = true;
-        console.log('haha you lose! You fell into a hole!');
+        console.log('You fell into a hole! You lose.');
     } else if (myField[x][y] === hat) {
         win = true;
-        console.log('you win!');
+        console.log('you win! Well done.');
     } 
 }
 
 //core game method. Handles user input, and updates the current location.
 function runGame() {
-    //start in top left corner. Assigning global variables to x and y.
+
+    /* start in top left corner. Assigning global variables to x and y so they can 
+    be used in the helper methods.*/
     x = 0;
     y = 0;
     myField[x][y] = pathCharacter;
@@ -105,12 +126,6 @@ function runGame() {
                 console.log('please enter either u, d, l or r');
         }
     }
-
-
 }
 
-
-
 runGame();
-
-
